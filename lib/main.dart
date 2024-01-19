e0d031f84d77e47792a711f37b369ee72b4b75e5
luximo55 <96381767+luximo55@users.noreply.github.com>
2024-01-19 11:55:33 +0100

    Added blank screen to add money, still need actual functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 void main() {
-  runApp(MyApp());
+  runApp(const MaterialApp(
+    title: 'Money Pal',
+    home: MyApp(),
+  ));
 }
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -15,52 +18,78 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor:  Colors.red,
-          title: const Text(
-            'MoneyPal',
-            style: TextStyle(color: Colors.white),
-          ),
-        ),
-        body: Container(
-          child: Text(
-            'Trenutno stanje\n$count€',
-            style: TextStyle(fontSize: 30, color: Colors.white),
-          ),
-          color: Colors.red,
-          width: 1500,
-          height: 110,
-          margin: const EdgeInsets.all(10),
-          padding: const EdgeInsets.all(10),
-          transformAlignment: Alignment.centerLeft,
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: (){
-            setState(() {
-              count++;
-            });
-          },
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home_rounded),
-              label: 'Home',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.calendar_month_rounded),
-              label: 'Monthly View',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.settings),
-              label: 'Settings',
-            ),
-          ]
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor:  Colors.red,
+        title: const Text(
+          'MoneyPal',
+          style: TextStyle(color: Colors.white),
         ),
       ),
+      body: Column(
+        children: [
+          Container(
+            child: Text(
+              'Trenutno stanje\n$count€',
+              style: TextStyle(fontSize: 30, color: Colors.white),
+            ),
+            color: Colors.red,
+            width: 1500,
+            height: 110,
+            margin: const EdgeInsets.all(10),
+            padding: const EdgeInsets.all(10),
+            transformAlignment: Alignment.centerLeft,
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: (){
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const AddMoney()),
+          );
+        },
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_rounded),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_month_rounded),
+            label: 'Monthly View',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ]
+      ),
+    );
+  }
+}
+
+class AddMoney extends StatelessWidget {
+  const AddMoney({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor:  Colors.red,
+        title: const Text(
+          'Add amount',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: (){
+              Navigator.pop(context);;
+          },
+        ),
     );
   }
 }
